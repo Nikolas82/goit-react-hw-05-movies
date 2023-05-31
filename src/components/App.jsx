8cd7918d1cd3466b771c2a63b873ce2556a458fc
Nikolas82 <nikobogdan34@gmail.com>
2023-05-31 07:13:47 +0300

    update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,7 @@ export const App = () => {
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="*" element={<div>NotFound</div>} />
+        <Route path="*" element={<Home />} />
       </Route>
     </Routes>
   );
